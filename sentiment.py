4c9db2eed1631a8ca66c453e297c2ee05d8f85c6
sly <slyvester@542.digital>
2025-12-26 18:40:10 +0000

    Refactor sentiment analysis logic to streamline document handling and enhance error management

--- sentiment.py
+++ sentiment.py
@@ -14,28 +14,54 @@ def sentiment():
     key = app.config['LANGUAGE_SERVICE_KEY']
     
     if request.method == 'POST':
-       
         text = request.get_json()['text']
-        
-        input = {'documents': [
+        documents =  [
             { 'id': '1000', 'text': text}
-        ]}
+        ]
+        # results = without_sdk_lib(endpoint, key, documents)
+        results = with_sdk_lib(endpoint, key, documents)
         
-        headers = {
-            'Ocp-Apim-Subscription-Key': key,
-            'Content-type': 'application/json'
-        }
-        endpoint = endpoint + 'text/analytics/v3.0/sentiment'
-        response = requests.post(url=endpoint, headers=headers, json=input)
-        results = response.json()['documents'][0]['confidenceScores']
-        
-        results = {
+        return {
             "negative": results['negative'] * 100,
             "neutral": results['neutral'] * 100,
             "positive": results['positive'] * 100
         }
-        
-        return results
    
     
     return render_template('sentiment.html')
+
+def with_sdk_lib(endpoint, key, documents):
+    from azure.core.credentials import AzureKeyCredential
+    from azure.ai.textanalytics import TextAnalyticsClient
+    from azure.core.exceptions import AzureError
+    
+    try:
+        client = TextAnalyticsClient(endpoint, AzureKeyCredential(key))
+        response = client.analyze_sentiment(documents)
+        results = response[0]
+        # print(results.confidence_scores)
+    except AzureError as e:
+        print("error occured", e.message)
+    
+    return {
+            "negative": results.confidence_scores.negative,
+            "neutral": results.confidence_scores.neutral,
+            "positive": results.confidence_scores.positive
+        }
+def without_sdk_lib(endpoint, key, documents):
+        
+    input = {
+        'documents': documents
+    }
+        
+    headers = {
+            'Ocp-Apim-Subscription-Key': key,
+            'Content-type': 'application/json'
+        }
+    print(input)
+    endpoint = endpoint + 'text/analytics/v3.0/sentiment'
+    response = requests.post(url=endpoint, headers=headers, json=input)
+    
+    results = response.json()['documents'][0]['confidenceScores']
+    
+    return results
